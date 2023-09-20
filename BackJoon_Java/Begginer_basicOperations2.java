df0f9a06b95d3dff985eaf9e74f557ff615cbacb
kwonminji <minji6313@yahoo.com>
2023-09-20 15:07:31 +0900

    #1.AlgorithmStudy_시작

---
+++ BackJoon_Java/Begginer_basicOperations2.java
@@ -0,0 +1,19 @@
+package BackJoon_Java;
+
+import java.util.Scanner;
+
+public class Begginer_basicOperations2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int sub = a-b;
+		
+		System.out.println(sub);
+
+	}
+}
---
+++ BackJoon_Java/Begginer_basicOperations3.java
@@ -0,0 +1,22 @@
+package BackJoon_Java;
+
+import java.util.Scanner;
+
+
+public class Begginer_basicOperations3 {
+
+		public static void main(String[] args) {
+			
+			Scanner sc = new Scanner(System.in);
+			
+			Double a = sc.nextDouble();
+			Double b = sc.nextDouble();
+			
+			Double div = a / b;
+
+			System.out.println(div);
+
+
+	}
+
+}
