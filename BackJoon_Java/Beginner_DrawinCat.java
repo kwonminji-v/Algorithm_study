62d8ed4efbe07b3bf831ca9c94f88ee921c56932
kwonminji <minji6313@yahoo.com>
2023-09-21 12:15:26 +0900

    #3.BackJoon_고양이 그리기

---
+++ BackJoon_Java/Beginner_DrawinCat.java
@@ -0,0 +1,18 @@
+package BackJoon_Java;
+
+public class Beginner_DrawinCat {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( \')");
+		System.out.println("(  /  )");
+		System.out.print(" \\");
+		System.out.print("(");
+		System.out.print("__");
+		System.out.print(")");
+		System.out.print("|");
+
+	}
+
+}
