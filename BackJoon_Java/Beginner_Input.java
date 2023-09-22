7a06377c683a4dd7a96c42e580215cc53c597c86
kwonminji <minji6313@yahoo.com>
2023-09-22 20:32:11 +0900

    #4.BackJoon_입출력

---
+++ BackJoon_Java/Beginner_Input.java
@@ -0,0 +1,15 @@
+package BackJoon_Java;
+
+public class Beginner_Input {
+
+	public static void main(String[] args) {
+		
+		int answer = 13;
+		String userName = "minji_0323";
+		
+		System.out.println(answer);
+		System.out.println(userName);
+	
+	}
+
+}
